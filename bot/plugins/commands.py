ea6d11024a9fc0b7baf14a2188926106cd0c6c53
DEVAKRISHNA PRASOBH <78889898+thesilentdevka@users.noreply.github.com>
2021-07-25 16:58:24 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developer', url="https://t.me/thesilentninja"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developer', url='https://t.me/thesilentninja'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/thesilentdevka/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/thesilentninja')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
